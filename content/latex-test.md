7257fd8f6ba657ba4a7e7c6f20df25ddcd9cf77b
Fritz Mueller <fritzm@fritzm.org>
2014-06-15 22:54:51 -0700

    Basic site setup with Pelican + custom theme + latex

---
+++ content/latex-test.md
@@ -0,0 +1,5 @@
+Title: LaTeX test
+Date: 2014-06-15 18:40
+Category: Math
+
+Here's a LaTeX test: $$ \int\limits_M{\partial\omega} = \int\limits_{\partial M}{\omega} $$
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,3 +1,33 @@
-OUTPUT_PATH = '/Users/fritzm/Sites/fritzm.github.io'
-SITEURL = 'http://blog.fritzm.org'
-TIMEZONE = 'America/Los_Angeles'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Fritz Mueller'
+SITENAME = u'blog.fritzm.org'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PLUGIN_PATH = 'plugins'
+PLUGINS = ['render_math']
+THEME = './theme'
