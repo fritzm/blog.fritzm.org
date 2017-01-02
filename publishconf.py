c874e02ab7c790eef038a3a5e3439e476e17aebf
Fritz Mueller <fritzm@fritzm.org>
2017-01-01 18:22:03 -0800

    Update to Pelican 3.7; try RSS instead of ATOM

--- publishconf.py
+++ publishconf.py
@@ -14,8 +14,8 @@ SITEURL = 'http://fritzm.github.io'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
 
